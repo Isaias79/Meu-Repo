e191dd5363ee9b90f77ef9b943a54cf5f63232f5
Isaias79 <60967597+Isaias79@users.noreply.github.com>
2023-05-24 20:06:13 +0000

    feat abc

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "José Isaias",
+  title: "Aprendiz e estudante",
+  email: "isaiasprofe79@gmail.com",
+  gitHub: "isaias79",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
